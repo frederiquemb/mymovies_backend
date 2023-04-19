b6b9b39af9c45c5abf2607904b27b5baf3646780
frederiquembi <frederiquembime@gmail.com>
2023-04-19 16:44:11 +0200

    tet

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const api_key = process.env.OWM_API_KEY
+const apikey = process.env.OWM_API_KEY
 
 
 router.get('/movies', (req, res) => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`)
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apikey}`)
     .then(response => response.json())
     .then(data => {
       res.json({ movies: data.results});
